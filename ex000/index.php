85ca92485d98e1713a70e7cfffc9087f2fa65168
Filipe Alves <131316142+israelvalente@users.noreply.github.com>
2023-06-22 07:25:00 -0300

    update

---
+++ ex000/index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>php</title>
+</head>
+<body>
+    <h1>
+        <?php
+            echo "Jesus Salvador do \u{1F30E}";
+        ?>
+        <p>Troca de espírito.</p>
+    </h1>
+</body>
+</html>
