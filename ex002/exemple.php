4d6ad7927ebab9844e9105d2be0d28d3691ba5b7
Filipe Alves <131316142+israelvalente@users.noreply.github.com>
2023-07-08 20:10:10 -0300

    update

---
+++ ex002/exemple.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php 
+    $nome = "Gustavo";
+    $sobrenome = "Guanabara";
+    const PAIS = "Brasil";
+    $age = 43;
+    $weight = 118.5;
+    $marryed = false;
+
+    //PAIS = "Eua";
+    echo "Its a pleasure, $nome $sobrenome! Você mora no " . PAIS ; echo " You are $age, you have $weight kg and you $marryed"
+    ?>
+</body>
+</html>
