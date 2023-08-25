fc89591d1a1894a440c7d41596d32c2d8eddf4a4
Filipe Alves <131316142+israelvalente@users.noreply.github.com>
2023-08-25 18:07:30 -0300

    updates

---
+++ ex004/cad.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Result</title>
+</head>
+<body>
+    <header>
+        <h1>Result of the process</h1>
+    </header>
+    <main>
+        <?php 
+            var_dump($_GET);
+        
+        ?>
+    </main>
+</body>
+</html>
